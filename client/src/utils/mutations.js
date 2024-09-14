3817a86d113a8dfc738dec0d05bd15ab49e5f1f7
Lindsay Terry <lindsaytee66@gmail.com>
2024-09-14 17:58:54 -0600

    Adds server side auth and user model/graphql schema for logging in and creating a user

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,1 +1,23 @@
 import { gql } from '@apollo/client';
+
+export const LOGIN = gql`
+    mutation login($email: String, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+            }
+        }
+    }
+`;
+
+export const CREATE_USER = gql `
+    mutation createUser($firstName: String!, $lastName: String!, $email: String!, $password: String!) {
+        createUser(firstName: $firstName, lastName: $lastName, email: $email, password: $password) {
+            token
+            user {
+                _id
+            }
+        }
+    }    
+`;
