9f44d75993aa8e8b766e0fc6ad5decc93502d235
Elena Papanikolas <epapanikolas@hotmail.com>
2024-09-17 15:28:00 -0600

    Added signup feature.  Made changes to server to connect with .env.  Modified mutations to change type date to string.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,36 @@
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 
 function App() {
 
   return (
-    <div>
+    <ApolloProvider client={client}>
       <Header />
       <Outlet />
-    </div>
+    </ApolloProvider>
 );
 }
 
